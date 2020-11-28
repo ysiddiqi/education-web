1a0970dd7d4bdb86eb93140c2f06076408cd1578
Yas <ysiddiqi@users.noreply.github.com>
2020-11-28 18:18:28 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// function myFunction() {
+    // var x = document.getElementById("myTopnav");
+    // if (x.className === "navbar") {
+    //   x.className += " responsive";
+    // } else {
+    //   x.className = "navbar";
+    // }
+//   }
+
+ $(document).ready(function(){
+    $('[data-toggle="popover"]').popover();   
+  });
